656a144eac5b049ee14e52a25402737d1e63d4eb
Marcel Hellkamp <marc@gsites.de>
2011-03-31 15:01:14 +0200

    Changed start.sh to be more robust.

--- start.sh
+++ start.sh
@@ -2,24 +2,25 @@
 cd `dirname $0`
 
 # Update homepage
-git checkout master
-git pull
+#git checkout master
+#git pull
+git fetch
+git status
 
-# Copy bottle executable
-rm bottle.py
-wget --no-check-certificate https://github.com/defnull/bottle/raw/master/bottle.py
+# Download bottle module
+wget -O bottle.py --no-check-certificate https://github.com/defnull/bottle/raw/master/bottle.py
 
 #config
-ssdopts="--background --startas ./app.py --group www-data --chuid www-data --make-pidfile"
-ssdopts="$ssdopts --chdir `pwd`"
 
-#Start 4 instances of the server (8080-8084)
+# Start 4 new processes
 for n in 0 1 2 3; do
+  pidfile="/var/run/bottlepy.$n.pid"
   echo "$n: Killing old instance..."
-  pid="/var/run/bottle.$n.pid"
-  kill `cat $pid` &>/dev/null && sleep 1
-  echo "$n: Starting new one..."
-  start-stop-daemon --start $ssdopts --pidfile $pid -- "808$n"
+  start-stop-daemon --stop  --pidfile $pidfile --group www-data --chuid www-data
+  echo "$n: Starting new instance..."
+  start-stop-daemon --start --background --startas ./app.py \
+                            --group www-data --chuid www-data --chdir `pwd` \
+                            --make-pidfile --pidfile $pidfile -- "808$n"
 done
 echo 'Done'
 
