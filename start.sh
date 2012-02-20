f53c65542e2c2ef48766b746d266ab9e0bad5ad2
Marcel Hellkamp <marc@gsites.de>
2012-02-20 19:51:57 +0100

    Some fixes to allow server start as non-root.

--- start.sh
+++ start.sh
@@ -1,26 +1,17 @@
+#!/bin/bash
+set -e
+
 # Make sure we are in the right directory
 cd `dirname $0`
 
-# Update homepage
-#git checkout master
-#git pull
-git fetch
-git status
-
 # Download bottle module
-wget -O bottle.py --no-check-certificate https://github.com/defnull/bottle/raw/master/bottle.py
-
-#config
+wget -nv -O bottle.py --no-check-certificate http://bottlepy.org/bottle.py
 
 # Start 4 new processes
 for n in 0 1 2 3; do
-  pidfile="/var/run/bottlepy.$n.pid"
-  echo "$n: Killing old instance..."
-  /sbin/start-stop-daemon --stop  --pidfile $pidfile --group www-data --chuid www-data
-  echo "$n: Starting new instance..."
-  /sbin/start-stop-daemon --start --background --startas ./app.py \
-                            --group www-data --chuid www-data --chdir `pwd` \
-                            --make-pidfile --pidfile $pidfile -- "808$n"
+  pidfile="run-$n.pid"
+  /sbin/start-stop-daemon --stop --oknodo --pidfile $pidfile
+  /sbin/start-stop-daemon --start --background --startas ./app.py --chdir `pwd`  \
+                          --make-pidfile --pidfile $pidfile -- "808$n"
 done
-echo 'Done'
 
